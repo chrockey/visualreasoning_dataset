346b7e65cd79ba4545c9cac4066399851a16811d
root <jmlee24@postech.ac.kr>
2025-11-27 08:28:33 +0000

    initial commit for visual trace (add todo)

--- src/pipelines/visual_trace.py
+++ src/pipelines/visual_trace.py
@@ -2,10 +2,19 @@ from typing import Any, Dict
 
 from .base import BasePipeline, load_config
 
+# TODO : Implement the VisualTracePipeline
+# 1. Load the Gemma, CoTracker v3, Grounded-SAM2 models
+# 2. extract main object from the image using Gemma
+# 3. Grounded SAM2 to segment the main object and extract the keypoints
+# 4. track the keypoints using CoTracker v3
+# 5. filter the keypoints using the confidence score
 
 class VisualTracePipeline(BasePipeline):
     def __init__(self, config: Dict[str, Any]):
         super().__init__(config)
+        self.object_extractor = None
+        self.grounded_segmenter = None
+        self.keypoint_tracker = None
 
     def preprocess(self, data_dict: Dict[str, Any]):
         raise NotImplementedError
