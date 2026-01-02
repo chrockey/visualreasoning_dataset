3ab3c1c3678ae4de8602eb5ff1879e4c43e67165
dmsgk724 <dmsgk724@postech.ac.kr>
2026-01-02 16:06:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ MAX_JOBS=64 pip -v install --no-deps --no-build-isolation flash-attn
 
 pip install hydra-core einops accelerate iopath kagglehub
 pip install typing pathlib dataclasses
+
+git clone https://github.com/facebookresearch/sam3.git
+cd sam3 && pip install -e .
